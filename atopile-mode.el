241ee4e6ddad0616dea2ffe2c3309da7ad4c1551
William Emfinger <waemfinger@gmail.com>
2024-02-20 11:10:16 -0600

    comments

--- atopile-mode.el
+++ atopile-mode.el
@@ -11,18 +11,24 @@
 ;;; Code:
 (defvar atopile-font-lock-keywords)
 (setq atopile-font-lock-keywords `(
-                                   ("\\b\\(import\\|from\\)" . (1 font-lock-keyword-face))
-                                   ("\\.\\(footprint\\|value\\|pin\\|signal\\|mpn\\|configure\\|add\\|pad\\|out\\|in\\|pin\\|ground\\|power\\|gnd\\|vcc\\|footprint\\|mpn\\)\\($\\| \\)" . (1 font-lock-keyword-face))
                                    ("\\b\\(import\\|module\\|component\\|to\\|from\\|assert\\|new\\)\\b" . (1 font-lock-keyword-face))
-                                   ("\\b\\(with\\)" . (1 font-lock-keyword-face)) ;; keyword.control.statement.ato
+                                   ;; match .(footprint|value|pin|signal|mpn|configure|add|pad|out|in|pin|ground|power|gnd|vcc) and highlight the word
+                                   ("\\.\\(footprint\\|value\\|pin\\|signal\\|mpn\\|configure\\|add\\|pad\\|out\\|in\\|pin\\|ground\\|power\\|gnd\\|vcc\\)\\($\\| \\)" . (1 font-lock-keyword-face))
+                                   ;; match \b(with)\b and highlight the word
+                                   ("\\b\\(with\\)\\b" . (1 font-lock-keyword-face)) ;; keyword.control.statement.ato
+                                   ;; match \b(module|component|interface)\b and highlight the word
                                    ("\\(^\\s*\\(module\\|component\\|interface\\)\\s+\\([a-zA-Z_][a-zA-Z_0-9]*\\)\\)" . (1 font-lock-keyword-face)) ;; meta.class.ato - storage.type.class.ato entity.name.type.class.ato
                                    ;; ("\\b\\(new\\)\\s+\\(\\w+\\(?:\\.\\w*\\)?\\)" . (1 font-lock-keyword-face)) ;; keyword.operator.new.ato, entity.name.type.instance.ato
+                                   ;; match \b(pin|signal)\b and highlight the word
                                    ("\\b\\(pin\\|signal\\)" . (1 font-lock-keyword-face)) ;; storage.type.c
+                                   ;; match \b(assert) and highlight the word
                                    ("\\b\\(assert\\)" . (1 font-lock-keyword-face)) ;; keyword.control.ato
                                    ("\\b\\(if\\|while\\|for\\|return\\|optional\\)" . (1 font-lock-keyword-face)) ;; keyword.control.ato
                                    ("\"[^\"]*\"" . font-lock-string-face)
                                    ("\\b\\(True\\|False\\)" . (1 font-lock-constant-face))
+                                   ;; match [fpnuµmkKMG](ohm|F|H|Hz|A|V|W|S)\b and highlight the word
                                    ("\\([fpnuµmkKMG]\\(ohm\\|F\\|H\\|Hz\\|A\\|V\\|W\\|S\\)\\)\\b" . (1 font-lock-constant-face))
+                                   ;; match +/- [0-9]+% and highlight the word
                                    ("\\(+/- [0-9]+\\%\\)" . (1 font-lock-doc-face))
                                    ;; ("\\([0-9]+\\(\\.[0-9]+\\)?\\)\\([a-zA-Z]+\\|\\%\\)?" . (1 font-lock-type-face)) ;; constant.numeric.ato
                                    ("= \\([+-]?[0-9]+\\(\\.[0-9]+\\)?\\)" . (1 font-lock-warning-face))
